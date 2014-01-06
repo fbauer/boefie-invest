3eb55cdcefbdcec63ea4837c85d01a43b3ce33c2
Florian Bauer <fbauer.devel@gmail.com>
2014-01-06 15:37:55 +0100

    port first test case from python prototype

--- test/flo_invest/core_test.clj
+++ test/flo_invest/core_test.clj
@@ -1,7 +1,34 @@
 (ns flo-invest.core-test
   (:require [clojure.test :refer :all]
-            [flo-invest.core :refer :all]))
+            [flo-invest.core :refer :all]
+            [clojure.java.io :as io]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(defn load-testdata []
+  (let [datadir "/home/flo/geldanlage/aktienscreen_2013_10_05/data/morningstar/2013_10_05/"]
+    (group-by :isin
+              (for [f (file-seq (io/file datadir))
+                    :let [filename (.getName f)
+                          parts (clojure.string/split filename #" ")
+                          isin (parts 0)
+                          ]
+                    :when (and
+                           (.endsWith filename ".csv")
+                           (= 3 (count parts))
+                           (contains? #{["Balance" "Sheet.csv"]
+                                        ["Income" "Statement.csv"]
+                                        ["Key" "Ratios.csv"]} (subvec parts 1))
+                           ) ] {:isin isin :type ({"Balance" :balancesheet
+                                                   "Income" :incomestatement
+                                                   "Key" :keyratios} (parts 1))  :file f} ))))
+
+
+(deftest test-data-available
+  (let  [testdata (load-testdata)]
+    (testing "loading test data succeeds"
+      (is (not= testdata []))
+      (is (= 3 (count  (first (vals testdata)))))
+      )))
