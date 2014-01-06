eefae9206d00a39d7f68d5e69d9875866dfc3f84
Florian Bauer <fbauer.devel@gmail.com>
2014-01-06 15:48:09 +0100

    move the data loading function to src directory

--- src/flo_invest/core.clj
+++ src/flo_invest/core.clj
@@ -1,6 +1,18 @@
-(ns flo-invest.core)
+(ns flo-invest.core (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-dir [datadir]
+  (group-by :isin
+            (for [f (file-seq (io/file datadir))
+                  :let [filename (.getName f)
+                        parts (clojure.string/split filename #" ")
+                        isin (parts 0)
+                        ]
+                  :when (and
+                         (.endsWith filename ".csv")
+                         (= 3 (count parts))
+                         (contains? #{["Balance" "Sheet.csv"]
+                                      ["Income" "Statement.csv"]
+                                      ["Key" "Ratios.csv"]} (subvec parts 1))
+                         ) ] {:isin isin :type ({"Balance" :balancesheet
+                                                 "Income" :incomestatement
+                                                 "Key" :keyratios} (parts 1))  :file f})))
