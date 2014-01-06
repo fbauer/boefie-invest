e0dbdb4dc7a23eb7f4456c09f6b31859b6931aff
Florian Bauer <fbauer.devel@gmail.com>
2014-01-06 16:34:42 +0100

    interim step
    ported some of the python function for loading over to clojure

--- src/flo_invest/core.clj
+++ src/flo_invest/core.clj
@@ -1,4 +1,5 @@
-(ns flo-invest.core (:require [clojure.java.io :as io]))
+(ns flo-invest.core (:require [clojure.java.io :as io]
+                              [clojure.set :only select]))
 
 (defn parse-dir [datadir]
   (group-by :isin
@@ -16,3 +17,32 @@
                          ) ] {:isin isin :type ({"Balance" :balancesheet
                                                  "Income" :incomestatement
                                                  "Key" :keyratios} (parts 1))  :file f})))
+
+(defn load [file-data]
+  (let [balance_dict  {"Total current assets" :current_assets
+                       "Total current liabilities" :current_liabilities
+                       "Long-term debt" :long_term_debt
+                       "Total liabilities" :total_liabilities
+                       "Total assets" :total_assets
+                       "Goodwill" :goodwill
+                       "Intangible assets" :intangibles
+                       }
+        inputs  {:non_redeemable_preferred_stock 0
+                 :redeemable_preferred_stock 0
+                 :split_bonus_factor 1
+                 :goodwill Double/NaN
+                 :intangibles Double/NaN
+                 :annual_sales Double/NaN
+                 :current_assets Double/NaN
+                 :current_liabilities Double/NaN
+                 :long_term_debt Double/NaN
+                 :isin ((first file-data) :isin) 
+                 }
+        file-data-set (set file-data)
+        income     (clojure.set/select #(= (:type %) :incomestatement) file-data-set)
+        balance     (clojure.set/select #(= (:type %) :balancesheet) file-data-set)
+        keyratios     (clojure.set/select #(= (:type %) :keyratios) file-data-set)
+        ]
+
+    inputs
+  ))
--- test/flo_invest/core_test.clj
+++ test/flo_invest/core_test.clj
@@ -1,6 +1,7 @@
 (ns flo-invest.core-test
   (:require [clojure.test :refer :all]
             [flo-invest.core :refer :all]
+            [clojure.java.io :as io]
             ))
 
 
@@ -12,3 +13,27 @@
       (is (not= testdata []))
       (is (= 3 (count  (first (vals testdata)))))
       )))
+
+(deftest test_loading_of_hfc_stock
+  (let [testdata "/home/flo/geldanlage/aktienscreen_2013_10_05/data/morningstar/2013_10_05/"
+        input (load [{:isin "US4361061082" :type :balancesheet :file (io/file (str testdata  "US4361061082 Balance Sheet.csv")) }
+                     {:isin "US4361061082" :type :incomestatement :file (io/file (str testdata  "US4361061082 Income Statement.csv"))}
+                     {:isin "US4361061082" :type :keyratios :file (io/file (str testdata  "US4361061082 Key Ratios.csv"))}])]
+    (is (= input {:isin "US4361061082",
+                  :annual_sales 20091e6,
+                  :currency "USD",
+                  :current_assets 4470e6,
+                  :current_liabilities 1654e6,
+                  :dividends [0.06, 0.07, 0.1, 0.15, 0.23, 0.3, 0.3, 0.3, 0.34, 0.6],
+                  :eps [0.36, 0.65, 1.33, 2.29, 2.99, 1.19, 0.2, 0.97, 6.42, 8.38],
+                  :goodwill 2338e6,
+                  :intangibles 169e6,
+                  :long_term_debt 1336e6,
+                  :non_redeemable_preferred_stock 0.0e6,
+                  :redeemable_preferred_stock 0.0e6,
+                  :reported_book_value 29.74,
+                  :shares_outstanding 206e6,
+                  :split_bonus_factor 1.0,
+                  :tangible_book_value 17.57009708737864,
+                  :total_assets 10329.0e6,
+                  :total_liabilities 4276e6}))))
