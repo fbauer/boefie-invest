821761e255911346a3f579d5088de830b8ec0f16
Florian Bauer <fbauer.devel@gmail.com>
2014-01-09 16:05:26 +0100

    progress

--- test/flo_invest/core_test.clj
+++ test/flo_invest/core_test.clj
@@ -22,27 +22,46 @@
 (deftest test-loading-of-hfc-stock
   (let [testdata "/home/flo/geldanlage/aktienscreen_2013_10_05/data/morningstar/2013_10_05/"
         input (load-data [{:isin "US4361061082" :type :balancesheet :file (io/file (str testdata  "US4361061082 Balance Sheet.csv")) }
-                     {:isin "US4361061082" :type :incomestatement :file (io/file (str testdata  "US4361061082 Income Statement.csv"))}
-                     {:isin "US4361061082" :type :keyratios :file (io/file (str testdata  "US4361061082 Key Ratios.csv"))}])
+                          {:isin "US4361061082" :type :incomestatement :file (io/file (str testdata  "US4361061082 Income Statement.csv"))}
+                          {:isin "US4361061082" :type :keyratios :file (io/file (str testdata  "US4361061082 Key Ratios.csv"))}])
 
-        expected {:isin "US4361061082",
-                    :annual_sales 20091e6,
-                    :currency "USD",
-                    :current_assets 4470e6,
-                    :current_liabilities 1654e6,
-                    :dividends [0.06, 0.07, 0.1, 0.15, 0.23, 0.3, 0.3, 0.3, 0.34, 0.6],
-                    :eps [0.36, 0.65, 1.33, 2.29, 2.99, 1.19, 0.2, 0.97, 6.42, 8.38],
-                    :goodwill 2338e6,
-                    :intangibles 169e6,
-                    :long_term_debt 1336e6,
-                    :non_redeemable_preferred_stock 0.0e6,
-                    :redeemable_preferred_stock 0.0e6,
-                    :reported_book_value 29.74,
-                    :shares_outstanding 206e6,
-                    :split_bonus_factor 1.0,
-                    :tangible_book_value 17.57009708737864,
-                    :total_assets 10329.0e6,
-                    :total_liabilities 4276e6}]
+        expected {:isin "US4361061082"
+                  :annual_sales (as-money "20091000000" "USD")
+                  :currency "USD"
+                  :current_assets (as-money "4470000000" "USD")
+                  :current_liabilities (as-money "1654000000" "USD")
+                  :dividends [(as-money "0.06" "USD")
+                              (as-money "0.07" "USD")
+                              (as-money "0.1" "USD")
+                              (as-money "0.15" "USD")
+                              (as-money "0.23" "USD")
+                              (as-money "0.3" "USD")
+                              (as-money "0.3" "USD")
+                              (as-money "0.3" "USD")
+                              (as-money "0.34" "USD")
+                              (as-money "0.6" "USD")]
+                  :eps [(as-money "0.36" "USD")
+                        (as-money "0.65" "USD")
+                        (as-money "1.33" "USD")
+                        (as-money "2.29" "USD")
+                        (as-money "2.99" "USD")
+                        (as-money "1.19" "USD")
+                        (as-money "0.2" "USD")
+                        (as-money "0.97" "USD")
+                        (as-money "6.42" "USD")
+                        (as-money "8.38" "USD")]
+                  :goodwill (as-money "2338000000" "USD")
+                  :intangibles (as-money "169000000" "USD")
+                  :long_term_debt (as-money "1336000000" "USD")
+                  :non_redeemable_preferred_stock 0.0000000
+                  :redeemable_preferred_stock 0.0000000
+                  :reported_book_value (as-money "29.74" "USD")
+                  :shares_outstanding 206000000.0
+                  :split_bonus_factor 1.0
+                  :tangible_book_value (as-money "17.57009708737864" "USD")
+                  :total_assets (as-money "10329000000" "USD")
+                  :total_liabilities (as-money "4276000000" "USD")
+                  }]
     (testing "Result from calling load-data is as expected"
       (is (= (diff input expected)  [nil nil expected]))
       )))
@@ -84,16 +103,20 @@
 
 (deftest  compare-with-python-results
   (let [py-data (binding [factory/*json-factory*
-                           (factory/make-json-factory {:allow-non-numeric-numbers true})]
-                   (parse-stream (clojure.java.io/reader
-                                  "/home/flo/geldanlage/aktienscreen_2013_10_05/morningstar.json")))
+                          (factory/make-json-factory {:allow-non-numeric-numbers true})]
+                  (parse-stream (clojure.java.io/reader
+                                 "/home/flo/geldanlage/aktienscreen_2013_10_05/morningstar.json")))
         clj-data (vec (for [input (parse-dir "/home/flo/geldanlage/aktienscreen_2013_10_05/data/morningstar/2013_10_05/" )] (load-data (last input))))
+
+        py-convert (fn [py key] (py key))
         py-keys [:isin :currency :annual_sales :current_assets :current_liabilities
                  :dividends :eps :goodwill :intangibles :long_term_debt
                  :non_redeemable_preferred_stock :redeemable_preferred_stock
                  :reported_book_value :shares_outstanding :split_bonus_factor
-                 :tangible_book_value :total_assets :total_liabilities]]
-    (doseq [[py clj] (map list  (sort-by :isin (map #(zipmap py-keys %) py-data)) (sort-by :isin clj-data))
+                 :tangible_book_value :total_assets :total_liabilities]
+        py-map (map #(zipmap py-keys %) py-data)
+        ]
+    (doseq [[py clj] (map list (sort-by :isin py-map) (sort-by :isin clj-data))
             key py-keys]
-      (is (eq (py key) (clj key)) (str (py :isin) key)))
+      (is (eq (py-convert py key) (clj key)) (str (py :isin) key)))
     ))
