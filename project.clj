be067ec68642ecc50b2ca5420b22906fac9d4780
Florian Bauer <fbauer.devel@gmail.com>
2014-01-10 16:04:40 +0100

    Add Marginalia documentation tool.

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
                  [clojure-csv/clojure-csv "2.0.1"]
                  [cheshire "5.3.0"]
                  [org.joda/joda-money "0.8"]
-                 ])
+                 ]
+  :plugins [[lein-marginalia "0.7.1"]])
