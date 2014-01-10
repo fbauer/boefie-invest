bde29c46dab1af7cb7cafd2cab327e3df285ca83
Florian Bauer <fbauer.devel@gmail.com>
2014-01-10 15:41:41 +0100

    Update the README and license information.

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # flo-invest
 
-A Clojure library designed to ... well, that part is up to you.
+A program to do security analysis following Graham's ideas.
 
 ## Usage
 
-FIXME
+
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright © 2014 Florian Bauer
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject flo-invest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Security analysis according to Graham"
+  :url ""
+  :license {:name ""
+            :url ""}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [cheshire "5.3.0"]
