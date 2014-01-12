66cf2d33732d814337818d42feadaa200532dc53
Florian Bauer <fbauer.devel@gmail.com>
2014-01-12 13:37:01 +0100

    add documentation

--- src/flo_invest/database.clj
+++ src/flo_invest/database.clj
@@ -11,8 +11,23 @@
 (defn dbg [a] (do (print a) a))
 
 (defn init-db [db-spec]
+  "Create all tables in the database specified by db-spec.
+
+  Assumes that none of the tables exists before."
   (sql/db-do-commands
    db-spec
+   ;; The securities table is used to map an ISIN to the name of a
+   ;; security. It is append-only. An ISIN can never change, but the
+   ;; name of the corresponding security might. Therefore, the table
+   ;; has an additional column date_added which stores the date a
+   ;; column has been added. For equal ISINs, the most recently added
+   ;; row is the one that should be returned on a query.
+   ;;
+   ;; Sqlite implementation note:
+   ;; As sqlite has no native datetime column type, we store
+   ;; date_added with text affinity. The value is expected to be an
+   ;; utf-8 encoded string representation in iso datetime format and
+   ;; utc timezone.
    (sql/create-table-ddl
     :securities
     [:id "integer not null primary key autoincrement"]
